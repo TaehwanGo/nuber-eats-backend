a354a7613365617125ba9cf5177ea5aa6fae2e92
TaehwanGo <gth1123@naver.com>
2021-01-04 21:59:53 +0900

    #3.1 Data mapper vs Active Record

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entitiy';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       ignoreEnvFile: process.env.NODE_ENV === 'prod', // 서버에 deploy할때 .env파일(environment variable파일)을 사용하지 않겠다는 것
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
-        DB_HOST: Joi.string().required(), // 어떻게 .env파일에 있는 변수들을 process.env. 를 붙이지 않고 바로 쓸 수 있는거지?
+        DB_HOST: Joi.string().required(), // 어떻게 .env파일에 있는 변수들을 process.env. 를 붙이지 않고 바로 쓸 수 있는거지? ConfigModule의 validationSchema에서 Joi 패키지를 사용하면 가능
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
@@ -28,8 +29,9 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true, // TypeORM이 데이터베이스에 연결할 때 DB를 나의 모듈의 현재 상태로 마이그레이션 한다는 뜻
+      synchronize: process.env.NODE_ENV !== 'prod', // TypeORM이 데이터베이스에 연결할 때 DB를 나의 모듈의 현재 상태로 마이그레이션 한다는 뜻
       logging: true,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true, // 메모리에 자동으로 만들어 져서 schema 파일을 따로 안만들어도 되게 하는 설정(code first & typescript라서 가능) // 아니면 이런식으로 파일이 만들어짐 autoSchemaFile: join(process.cwd(), 'src/schema.gql')
--- src/restaurants/entities/restaurant.entitiy.ts
+++ src/restaurants/entities/restaurant.entitiy.ts
@@ -1,19 +1,48 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// @ObjectType()
+// export class Restaurant {
+//   // Restaurant의 objectType
+//   // 아직 DB는 없지만 모든 특징들을 살펴본뒤 DB를 추가 할 것임(TypeORM)
+//   // GraphQL 관점에서 본 Restaurnat가 어떻게 생겼는지 묘사 할 것임
+//   @Field(() => String)
+//   name: string;
+
+//   @Field(() => Boolean)
+//   isVegan: boolean;
+
+//   @Field(() => String)
+//   address: string;
+
+//   @Field(() => String)
+//   ownerName: string;
+// }
 
 @ObjectType()
+@Entity()
 export class Restaurant {
-  // Restaurant의 objectType
-  // 아직 DB는 없지만 모든 특징들을 살펴본뒤 DB를 추가 할 것임(TypeORM)
-  // GraphQL 관점에서 본 Restaurnat가 어떻게 생겼는지 묘사 할 것임
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
   @Field(() => String)
+  @Column()
   name: string;
 
   @Field(() => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(() => String)
+  @Column()
   address: string;
 
   @Field(() => String)
+  @Column()
   ownerName: string;
+
+  @Field(() => String)
+  @Column()
+  categoryName: string;
 }

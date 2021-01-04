b4ff81036284126968144e503155fed84701e73f
TaehwanGo <gth1123@naver.com>
2021-01-04 17:18:30 +0900

    #2.3 PostgreSQL, TypeORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule, // resolver module
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345', // localhost에선 안써도 됨
+      database: 'nuber-eats',
+      synchronize: true, // TypeORM이 데이터베이스에 연결할 때 DB를 나의 모듈의 현재 상태로 마이그레이션 한다는 뜻
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true, // 메모리에 자동으로 만들어 져서 schema 파일을 따로 안만들어도 되게 하는 설정(code first & typescript라서 가능) // 아니면 이런식으로 파일이 만들어짐 autoSchemaFile: join(process.cwd(), 'src/schema.gql')
     }),
-    RestaurantsModule, // resolver module
   ],
   controllers: [],
   providers: [],

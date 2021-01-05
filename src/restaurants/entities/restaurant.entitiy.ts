7fad6431db9d93ebafcb196b7c16d3d7bf5a49b5
TaehwanGo <gth1123@naver.com>
2021-01-06 01:53:01 +0900

    #3.6 Optional Types and Columns

--- src/restaurants/entities/restaurant.entitiy.ts
+++ src/restaurants/entities/restaurant.entitiy.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 // @InputType({isAbstract: true}) // isAbstract: true는 InputType이 스키마에 포함되지 않는 다는 뜻 : 직접 사용하는 게 아닌 확장시킨다는 말(이해 못 함)
@@ -15,9 +15,10 @@ export class Restaurant {
   @IsString()
   name: string;
 
-  @Field(() => Boolean)
-  @Column()
+  @Field(() => Boolean, { nullable: true, defaultValue: false })
+  @Column({ default: false })
   @IsBoolean()
+  @IsOptional()
   isVegan: boolean;
 
   @Field(() => String)
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -21,6 +21,7 @@ export class RestaurantsResolver {
   ): Promise<boolean> {
     // Args를 하나하나 적어주는 것을 받는 것과 똑같은데 여기서 볼땐 더 깔끔하게 보임
     // 여전히 data가 잘 저장 됐는지를 boolean으로 return 하기 위해 async await을 사용
+    console.log(createRestaurantDTO);
     try {
       await this.restaurantService.createRestaurant(createRestaurantDTO);
       return true;

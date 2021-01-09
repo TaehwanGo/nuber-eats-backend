91ed1d7f6242579f2e7c31ae5bf34d2420a043cf
TaehwanGo <gth1123@naver.com>
2021-01-10 02:03:12 +0900

    #5.4 JWT Module part two

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,4 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { JwtService } from './jwt.service';
 
 @Module({})
-export class JwtModule {}
+@Global() // global module로 만들면 사용할 모듈(users.module)에서 providers에 추가 안해도 됨
+export class JwtModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
+    // 어떻게 options를 JwtService로 보낼까?
+    // DynamicModule은 다른 module을 return하는 module임
+    return {
+      module: JwtModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        JwtService,
+      ],
+      exports: [JwtService], // 우리의 목적은 JwtModule만 import하면 JwtService도 사용 가능하게 하는 것(users.module에서)
+    };
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config'; // ComfigModule을 global로 설정했기 때문에 providers에 등록하지 않고 사용할 수 있음
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigService], // service에 필요한 repository를 등록(TypeOrmModule)
+  imports: [TypeOrmModule.forFeature([User])], // service에 필요한 repository를 등록(TypeOrmModule)
   providers: [UsersResolver, UsersService], // app.module에서 ConfigModule을 import하면 ConfigService를 사용할 수 있음
 })
 export class UsersModule {}

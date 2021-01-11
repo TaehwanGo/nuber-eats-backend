2c7ca46acc0b3ab750cc717d0cf9c34e49795a76
TaehwanGo <gth1123@naver.com>
2021-01-12 00:59:27 +0900

    #6.6 Mail Module Setup

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,21 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Module({})
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
+    // 어떻게 options를 JwtService로 보낼까?
+    // DynamicModule은 다른 module을 return하는 module임
+    return {
+      module: MailModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+      ],
+      exports: [], // 우리의 목적은 JwtModule만 import하면 JwtService도 사용 가능하게 하는 것(users.module에서)
+    };
+  }
+}

cb550c8168029d02c0c196621ba41e890ba1ab45
TaehwanGo <gth1123@naver.com>
2021-01-21 02:15:09 +0900

    #10.9 Edit Restaurant part Three, custom respository

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entitiy';
+import { CategoryRepository } from './repositories/category.repository';
 import { RestaurantsResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
   //**2. forFeature는 TypeOrmModule이 특정feature(Restaurant entity)를 import 할 수 있게 해줌
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])], // entity가 여러개라면 여러개 import 가능
+  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])], // entity가 여러개라면 여러개 import 가능
   // graphql은 post방식으로 query를 받기 때문에 controller가 없는 것 같다.
   providers: [RestaurantsResolver, RestaurantService], // provider == service, graphql에선 서비스(구체적 구현부)가 resolver
 })
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -12,17 +12,18 @@ import {
 } from './dtos/edit-restaurant.dto';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entitiy';
+import { CategoryRepository } from './repositories/category.repository';
 
 @Injectable()
 export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant) // Repository with entity
     private readonly restaurants: Repository<Restaurant>, // **4. Repository class를 사용해서 DB에 접근(Data mapper 방식)
-    @InjectRepository(Category)
-    private readonly categories: Repository<Category>,
+    // @InjectRepository(Category)
+    private readonly categories: CategoryRepository, // Repository<Category> 를 상속받아서 만든 custom repository
   ) {} // 우리 Repository를 inject
 
-  async getOrCreateCategory(name: string): Promise<Category> {
+  async getOrCreate(name: string): Promise<Category> {
     const categoryName = name.trim().toLowerCase().replace(/ +/g, ' '); // 정규식 공부 필요 !
     // trim() : 앞, 뒤 space를 지워줌
     const categorySlug = categoryName.replace(/ /g, '-'); // '/ /g':regular expression
@@ -43,7 +44,7 @@ export class RestaurantService {
       const newRestaurant = this.restaurants.create(createRestaurantInput); // entity에 dto를 하나하나 씩 넣을 필요 없이 이렇게 해결 // 하지만 단지 typescript상에만 존재하는 클래스를 만든 것
       newRestaurant.owner = owner; // dto에서 owner는 graphql에서 받아오지 않기 때문에 여기서 완성시켜 줘야함
 
-      const category = await this.getOrCreateCategory(
+      const category = await this.categories.getOrCreate(
         createRestaurantInput.categoryName,
       );
 
@@ -81,7 +82,20 @@ export class RestaurantService {
           error: "You can't edit a restaurant that you don't own",
         };
       }
-
+      let category: Category = null; // default로 null을 넣어줌 (category를 유저가 변경 안할 수 도 있으므로)
+      if (editRestaurantInput.categoryName) {
+        category = await this.categories.getOrCreate(
+          editRestaurantInput.categoryName,
+        );
+      }
+      await this.restaurants.save([
+        {
+          // this.restaurants.create({})를 하지 않고 entity를 만들어서 save하고 싶으면 save()안에 []가 들어가야 함
+          id: editRestaurantInput.restaurantId,
+          ...editRestaurantInput,
+          ...(category && { category }), // category가 존재하면 category object를 리턴하는 것 : ...은 {}를 제거하고 결론적으로 category: category 가 됨
+        },
+      ]);
       // defensive programming : 처음에 에러를 핸들해주고 이후에 하고 싶은 것을 하는 것
       return { ok: true };
     } catch (error) {

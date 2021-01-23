bc8e3a3c8984c95ec245840a9609b295bacdf747
TaehwanGo <gth1123@naver.com>
2021-01-23 22:31:00 +0900

    #11.7 Create Order part Two

--- src/orders/dtos/create-order.dto.ts
+++ src/orders/dtos/create-order.dto.ts
@@ -1,13 +1,26 @@
 import { Field, InputType, Int, ObjectType, PickType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
-import { Dish } from 'src/restaurants/entities/dish.endtity';
+import { Dish, DishOption } from 'src/restaurants/entities/dish.endtity';
 import { Order } from '../entities/order.entity';
 
 @InputType()
-export class CreateOrderInput extends PickType(Order, ['items']) {
+class CreateOrderItemInput {
+  @Field(type => Int)
+  dishId: number;
+
+  @Field(type => [DishOption], { nullable: true })
+  options?: DishOption[];
+}
+
+@InputType()
+export class CreateOrderInput {
+  //  extends PickType(Order, ['items']) 고객이 주문하는데 dish의 모든 정보를 입력해야 될 필요는 없음
   // order를 만들때 dish의 모든 정보는 필요 없음 - name, price만 필요 : 이 다음 commit에서 확인
   @Field(type => Int)
   restaurantId: number;
+
+  @Field(type => [CreateOrderItemInput])
+  items: CreateOrderItemInput[];
 }
 
 @ObjectType()
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entitiy';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -8,5 +11,28 @@ export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
   ) {}
+  async createOrderInput(
+    customer: User,
+    { restaurantId, items }: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found.',
+        };
+      }
+      return { ok: true };
+    } catch (err) {
+      console.log(err);
+      return {
+        ok: false,
+        error: 'Could not create order.',
+      };
+    }
+  }
 }
